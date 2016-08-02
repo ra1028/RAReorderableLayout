86a6033833ceebd6f4eed9458bb8e1da96ce3a49
dimakomar <kmr.dma@gmail.com>
2016-08-02 15:30:35 +0300

    swift3 working migration, moved from @objc protocols to swifty protocols with protocol extentions, updated demo for swift3

--- RAReorderableLayout-Demo/RAReorderableLayout-Demo/VerticalViewController.swift
+++ RAReorderableLayout-Demo/RAReorderableLayout-Demo/VerticalViewController.swift
@@ -19,7 +19,7 @@ class VerticalViewController: UIViewController, RAReorderableLayoutDelegate, RAR
         super.viewDidLoad()
         self.title = "RAReorderableLayout"
         let nib = UINib(nibName: "verticalCell", bundle: nil)
-        collectionView.registerNib(nib, forCellWithReuseIdentifier: "cell")
+        collectionView.register(nib, forCellWithReuseIdentifier: "cell")
         collectionView.delegate = self
         collectionView.dataSource = self
         
@@ -42,34 +42,34 @@ class VerticalViewController: UIViewController, RAReorderableLayoutDelegate, RAR
     
     // RAReorderableLayout delegate datasource
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let screenWidth = CGRectGetWidth(UIScreen.mainScreen().bounds)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main().bounds.width
         let threePiecesWidth = floor(screenWidth / 3.0 - ((2.0 / 3) * 2))
         let twoPiecesWidth = floor(screenWidth / 2.0 - (2.0 / 2))
-        if indexPath.section == 0 {
-            return CGSizeMake(threePiecesWidth, threePiecesWidth)
+        if (indexPath as NSIndexPath).section == 0 {
+            return CGSize(width: threePiecesWidth, height: threePiecesWidth)
         }else {
-            return CGSizeMake(twoPiecesWidth, twoPiecesWidth)
+            return CGSize(width: twoPiecesWidth, height: twoPiecesWidth)
         }
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 2.0
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 2.0
     }
     
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsetsMake(0, 0, 2.0, 0)
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if section == 0 {
             return imagesForSection0.count
         }else {
@@ -77,44 +77,48 @@ class VerticalViewController: UIViewController, RAReorderableLayoutDelegate, RAR
         }
     }
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("verticalCell", forIndexPath: indexPath) as! RACollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "verticalCell", for: indexPath) as! RACollectionViewCell
         
-        if indexPath.section == 0 {
-            cell.imageView.image = imagesForSection0[indexPath.item]
+        if (indexPath as NSIndexPath).section == 0 {
+            cell.imageView.image = imagesForSection0[(indexPath as NSIndexPath).item]
         }else {
-            cell.imageView.image = imagesForSection1[indexPath.item]
+            cell.imageView.image = imagesForSection1[(indexPath as NSIndexPath).item]
         }
         return cell
     }
     
-    func collectionView(collectionView: UICollectionView, allowMoveAtIndexPath indexPath: NSIndexPath) -> Bool {
-        if collectionView.numberOfItemsInSection(indexPath.section) <= 1 {
+    func collectionView(_ collectionView: UICollectionView, allowMoveAt indexPath: IndexPath) -> Bool {
+        if collectionView.numberOfItems(inSection: (indexPath as NSIndexPath).section) <= 1 {
             return false
         }
         return true
     }
     
-    func collectionView(collectionView: UICollectionView, atIndexPath: NSIndexPath, didMoveToIndexPath toIndexPath: NSIndexPath) {
+    func collectionView(_ collectionView: UICollectionView, at: IndexPath, willMoveTo toIndexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, at atIndexPath: IndexPath, didMoveTo toIndexPath: IndexPath) {
         var photo: UIImage
-        if atIndexPath.section == 0 {
-            photo = imagesForSection0.removeAtIndex(atIndexPath.item)
+        if (atIndexPath as NSIndexPath).section == 0 {
+            photo = imagesForSection0.remove(at: (atIndexPath as NSIndexPath).item)
         }else {
-            photo = imagesForSection1.removeAtIndex(atIndexPath.item)
+            photo = imagesForSection1.remove(at: (atIndexPath as NSIndexPath).item)
         }
         
-        if toIndexPath.section == 0 {
-            imagesForSection0.insert(photo, atIndex: toIndexPath.item)
+        if (toIndexPath as NSIndexPath).section == 0 {
+            imagesForSection0.insert(photo, at: (toIndexPath as NSIndexPath).item)
         }else {
-            imagesForSection1.insert(photo, atIndex: toIndexPath.item)
+            imagesForSection1.insert(photo, at: (toIndexPath as NSIndexPath).item)
         }
     }
     
-    func scrollTrigerEdgeInsetsInCollectionView(collectionView: UICollectionView) -> UIEdgeInsets {
+    func scrollTrigerEdgeInsetsInCollectionView(_ collectionView: UICollectionView) -> UIEdgeInsets {
         return UIEdgeInsetsMake(100.0, 100.0, 100.0, 100.0)
     }
     
-    func collectionView(collectionView: UICollectionView, reorderingItemAlphaInSection section: Int) -> CGFloat {
+    func collectionView(_ collectionView: UICollectionView, reorderingItemAlphaInSection section: Int) -> CGFloat {
         if section == 0 {
             return 0
         }else {
@@ -122,7 +126,7 @@ class VerticalViewController: UIViewController, RAReorderableLayoutDelegate, RAR
         }
     }
     
-    func scrollTrigerPaddingInCollectionView(collectionView: UICollectionView) -> UIEdgeInsets {
+    func scrollTrigerPaddingInCollectionView(_ collectionView: UICollectionView) -> UIEdgeInsets {
         return UIEdgeInsetsMake(collectionView.contentInset.top, 0, collectionView.contentInset.bottom, 0)
     }
 }
@@ -131,9 +135,9 @@ class RACollectionViewCell: UICollectionViewCell {
     var imageView: UIImageView!
     var gradientLayer: CAGradientLayer?
     var hilightedCover: UIView!
-    override var highlighted: Bool {
+    override var isHighlighted: Bool {
         didSet {
-            hilightedCover.hidden = !highlighted
+            hilightedCover.isHidden = !isHighlighted
         }
     }
     
@@ -151,26 +155,26 @@ class RACollectionViewCell: UICollectionViewCell {
     
     private func configure() {
         imageView = UIImageView()
-        imageView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
         addSubview(imageView)
         
         hilightedCover = UIView()
-        hilightedCover.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        hilightedCover.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         hilightedCover.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        hilightedCover.hidden = true
+        hilightedCover.isHidden = true
         addSubview(hilightedCover)
     }
     
-    private func applyGradation(gradientView: UIView!) {
+    private func applyGradation(_ gradientView: UIView!) {
         gradientLayer?.removeFromSuperlayer()
         gradientLayer = nil
         
         gradientLayer = CAGradientLayer()
         gradientLayer!.frame = gradientView.bounds
         
-        let mainColor = UIColor(white: 0, alpha: 0.3).CGColor
-        let subColor = UIColor.clearColor().CGColor
+        let mainColor = UIColor(white: 0, alpha: 0.3).cgColor
+        let subColor = UIColor.clear().cgColor
         gradientLayer!.colors = [subColor, mainColor]
         gradientLayer!.locations = [0, 1]
         
